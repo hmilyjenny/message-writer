c4f16bdd4e7c481d4158194a8065ba13352cc374
Dejan Mijic <dejan@mainflux.com>
2017-08-24 13:39:15 +0200

    Redefine DB schema to use SenML
    Instead of storing binary encoded data, messages will be written as
    SenML properties passed through NATS topic. Note that the numerical
    values will have 0 as their default.
    
    Signed-off-by: Dejan Mijic <dejan@mainflux.com>

--- messages.go
+++ messages.go
@@ -4,10 +4,24 @@ package writer
 // Message represents a message emitted by the mainflux adapters layer.
 type Message struct {
 	Channel     string
-	ContentType string
 	Publisher   string
 	Protocol    string
-	Payload     []byte
+	BaseName    string  `json:"bn,omitempty"`
+	BaseTime    float64 `json:"bt,omitempty"`
+	BaseUnit    string  `json:"bu,omitempty"`
+	BaseValue   float64 `json:"bv,omitempty"`
+	BaseSum     float64 `json:"bs,omitempty"`
+	Version     int     `json:"bver,omitempty"`
+	Name        string  `json:"n,omitempty"`
+	Unit        string  `json:"u,omitempty"`
+	Value       float64 `json:"v,omitempty"`
+	StringValue string  `json:"vs,omitempty"`
+	BoolValue   bool    `json:"vb,omitempty"`
+	DataValue   string  `json:"vd,omitempty"`
+	ValueSum    float64 `json:"s,omitempty"`
+	Time        float64 `json:"t,omitempty"`
+	UpdateTime  float64 `json:"ut,omitempty"`
+	Link        string  `json:"l,omitempty"`
 }
 
 // MessageRepository specifies a message persistence API.
